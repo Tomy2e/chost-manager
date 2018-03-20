62c503c7ccddcf8ed46bfa01664ece40d67f4980
Tomy2e <tomy2e@live.fr>
2018-03-20 21:21:14 +0100

    affichage des erreurs sous forme d'exceptions en mode développement

--- includes/DBmanager.class.php
+++ includes/DBmanager.class.php
@@ -10,7 +10,12 @@ class DBmanager
         {
             try {
                 self::$instance = new PDO('mysql:host=localhost;dbname='.BDD_NAME, BDD_USERNAME, BDD_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-                //$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                if(ENVIRONMENT == 'DEV')
+                {
+                    // affichage des erreurs dans un environnement de DEV
+                    self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                }
                 
             } catch (PDOException $e) {
                 print "Erreur !: " . $e->getMessage() . "<br/>";
