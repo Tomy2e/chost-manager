38701750930028d30492d98cfee1cf1487e35bfb
keskydi <37584104+keskydi@users.noreply.github.com>
2020-02-17 17:37:34 -0500

    Update config.example.php

--- includes/config.example.php
+++ includes/config.example.php
@@ -21,28 +21,28 @@ define('ENVIRONMENT', 'DEV');
     Possibilité d'utiliser un chemin fictif
     Slash final obligatoire!
 */
-define('NGINX_PATH', '/etc/nginx/sites-enabled/');
+define('NGINX_PATH', 'sys/');
 
 /*
     * Commande pour recharger la configuration nginx
     Laisser vide pour ne pas relancer la configuration
     PHP doit avoir les droits nécessaires !
 */
-define('NGINX_RELOAD', 'service nginx reload');
+define('NGINX_RELOAD', '');
 
 /* 
     * Chemin vers la config des pools PHP-FPM
     Possibilité d'utiliser un chemin fictif
     Slash final obligatoire!
 */
-define('PHP_PATH', '/etc/php5/fpm/pool.d/');
+define('PHP_PATH', 'sys/');
 
 /*
     * Chemin vers les sockets PHP
     Possibilité d'utiliser un chemin fictif
     Slash final obligatoire!
 */
-define('PHP_SOCKET_PATH', '/run/php/');
+define('PHP_SOCKET_PATH', 'sys/');
 
 /*
     * Commande pour recharger la configuration PHP-FPM
@@ -50,7 +50,7 @@ define('PHP_SOCKET_PATH', '/run/php/');
     PHP doit avoir les droits nécessaires !
 
 */
-define('PHP_RELOAD', 'service php5-fpm reload');
+define('PHP_RELOAD', '');
 
 /*
     * Commande pour ajouter un utilisateur Unix
@@ -58,20 +58,20 @@ define('PHP_RELOAD', 'service php5-fpm reload');
     PHP doit avoir les droits nécessaires (root) !
     - arguments : %username%, %homedir%
 */
-define('UNIX_USERADD', "useradd %username% -s /bin/false -p '*' -d %homedir%");
+define('UNIX_USERADD', "");
 
 /*
     * Commande pour supprimer un utilisateur Unix
     Laisser vide pour ne pas supprimer les utilisateurs
     - arguments : %username%
 */
-define('UNIX_USERDEL', 'userdel --remove %username%');
+define('UNIX_USERDEL', '');
 
 /*
     * Répertoire contenant les données utilisateur
     Slash final obligatoire!
 */
-define('USER_PATH', '/home/');
+define('USER_PATH', 'sys/');
 
 /*
     * Domaine utilisé pour héberger les sites hébergés
@@ -84,27 +84,27 @@ define('USER_DOMAIN', 'chost.io');
     Laisser vide pour ne pas ajouter un utilisateur FTP
     - arguments : %username%, %homedir%, %password%
 */
-define('FTP_USERADD', '(echo %password%; echo %password%) | pure-pw useradd %username% -u %username% -g %username% -d %homedir%');
+define('FTP_USERADD', '');
 
 /*
     * Commande pour modifier le mot de passe d'un utilisateur FTP
     Laisser vide pour ne pas autoriser la modification d'un mot de passe
     - arguments : %username%, %password%
 */
-define('FTP_PASSWD', '(echo %password%; echo %password%) | pure-pw passwd %username%');
+define('FTP_PASSWD', '');
 
 /*
     * Commande pour supprimer un utilisateur FTP
     Laisser vide pour ne pas autoriser la suppression d'un utilisateur FTP
     - arguments : %username%
 */
-define('FTP_USERDEL', 'pure-pw userdel %username%');
+define('FTP_USERDEL', '');
 
 /* 
     * Commande pour mettre à jour la DB FTP
     Laisser vide pour ne pas mettre à jour la DB FTP automatiquement
 */
-define('FTP_MKDB', 'pure-pw mkdb');
+define('FTP_MKDB', '');
 
 /*
     * Commande pour définir la taille maximale que peut utiliser un utilisateur FTP (quotas)
@@ -114,7 +114,7 @@ define('FTP_MKDB', 'pure-pw mkdb');
     Source : https://download.pureftpd.org/pub/pure-ftpd/doc/README.Virtual-Users
     - arguments : %username%, %sizeInMB%
 */
-define('FTP_QUOTAS', 'pure-pw usermod %username% -N %sizeInMB%');
+define('FTP_QUOTAS', '');
 
 /*
     * Activer ou pas le paiement par PayPal
@@ -136,7 +136,7 @@ define('EMAIL_FROM', 'noreply@chost.com');
 /* 
     * URL pointant vers le manager
 */
-define('SITE_URL', 'http://localhost/chost-manager/');
+define('SITE_URL', 'http://localhost/chost/');
 
 /*
     * URL pointant vers phpMyAdmin
